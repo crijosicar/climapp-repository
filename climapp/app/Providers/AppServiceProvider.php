8df9adb67768ecba39f7e1638cb3a2901a1a6fa1
crijosicar <crijosicar@hotmail.com>
2017-04-19 22:14:48 -0500

    Fix lumen

--- climapp/app/Providers/AppServiceProvider.php
+++ climapp/app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {
-        //
-    }
+	{
+    	if ($this->app->environment() == 'local') {
+        $this->app->register('Wn\Generators\CommandsServiceProvider');
+    	}
+	}
 }
