334db5e3394e8470a12945d16724089a00dbbb8d
Crijosicar <crijosicar@hotmail.com>
2017-05-01 17:53:35 -0500

    Debug

--- climapp/routes/web.php
+++ climapp/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
@@ -11,57 +11,61 @@
 |
 */
 $app->get('/', function () use ($app) {
-    return $app->version();
+	return $app->version();
 });
 
 $app->group(['prefix' => 'api/v1'], function($app){
 	$app->get('/', function () use ($app) {
-	    return $app->version();
-	});
-
-	/**
-	 * Routes for resource city
-	 */
-	$app->group(['prefix' => 'city'], function () use ($app) {
-		$app->get('/', 'CityController@getAll');
-                $app->get('getCityByCityName/{cityName}', 'CityController@getCityByCityName');
-		$app->post('getCityById/{id}', 'CityController@getCityById');
-                $app->post('addNewCity', 'CityController@addNewCity');
-                $app->post('updateCityById/{id}', 'CityController@updateCityById');
-	});
-
-
-	/**
-	 * Routes for resource city-person
-	 */
-	$app->group(['prefix' => 'city-person'], function () use ($app) {
-                $app->get('/','CityPersonController@getAll');
+		return $app->version();
 	});
 	
 	/**
-	 * Routes for resource person
-	 */
+	* Routes for resource city
+	*/
+	$app->group(['prefix' => 'city'], function () use ($app) {
+		$app->get('/', 'CityController@getAll');
+		$app->get('getCityByCityName/{cityName}', 'CityController@getCityByCityName');
+		$app->post('getCityById/{id}', 'CityController@getCityById');
+		$app->post('addNewCity', 'CityController@addNewCity');
+		$app->post('updateCityById/{id}', 'CityController@updateCityById');
+	});
+	
+
+	/**
+	* Routes for resource city-person
+	*/
+	$app->group(['prefix' => 'city-person'], function () use ($app) {
+		$app->get('/','CityPersonController@getAll');
+	});
+	
+	
+	/**
+	* Routes for resource person
+	*/
 	$app->group(['prefix' => 'person'], function () use ($app) {
 		$app->get('/', 'PeopleController@getAll');
 	});
-
+	
+	
 	/**
-	 * Routes for resource t-user
-	 */
+	* Routes for resource t-user
+	*/
 	$app->group(['prefix' => 't-user'], function () use ($app) {
 		$app->get('/', 'TUsersController@getAll');
 	});
-
+	
+	
 	/**
-	 * Routes for resource user-access
-	 */
+	* Routes for resource user-access
+	*/
 	$app->group(['prefix' => 'user-access'], function () use ($app) {
 		$app->get('/', 'UserAccessesController@getAll');
 	});
-
+	
+	
 	/**
-	 * Routes for resource value-list
-	 */
+	* Routes for resource value-list
+    */
 	$app->group(['prefix' => 'value-list'], function () use ($app) {
 		$app->get('/', 'ValueListsController@getAll');
 	});
